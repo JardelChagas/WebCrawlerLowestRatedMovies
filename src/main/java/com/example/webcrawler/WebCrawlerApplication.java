5c40b36e17ee1eafad8abed93bc66f6c3182ac01
jardel <jardelchags@gmail.com>
2021-09-17 13:08:52 -0300

    finish

--- src/main/java/com/example/webcrawler/WebCrawlerApplication.java
+++ src/main/java/com/example/webcrawler/WebCrawlerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebScrapingApplication {
+public class WebCrawlerApplication {
 
     public static void main(String[] args){
-        SpringApplication.run(WebScrapingApplication.class, args);
+        SpringApplication.run(WebCrawlerApplication.class, args);
     }
 
 }
